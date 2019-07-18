d4d3fe814245e231ff95efeb88dbf9fd99c530db
aayushdutt <aayushd100@gmail.com>
2019-07-18 08:53:50 +0530

    move from telebot to Telegraf, move token to env

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
-const TELEGRAM_BOT_TOKEN = '945298348:AAHa2OFEJQ-dvsS4gzE4EfWsAca8Y1wT7_c'
-const TeleBot = require('telebot');
+const Telegraf = require('telegraf')
 const htmlFetcher = require('./htmlFetcher')
 
-const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
 const friendsFetcher = new htmlFetcher('.info ul li:nth-of-type(3)', 'http://codeforces.com/profile/aayushdutt')
 const followersFetcher = new htmlFetcher('.AboutListItem.ContentViewsAboutListItem span.body_text span.main_text',
                                          'https://www.quora.com/profile/Aayush-Dutt-1')
-                                         
-bot.on('/check', async (msg) => {
-    msg.reply.text("checking for updates... ")
+
+
+//logging functions
+function userString(ctx) {
+    return JSON.stringify(ctx.from.id == ctx.chat.id ? ctx.from : {
+        from: ctx.from,
+        chat: ctx.chat
+    });
+}
+
+function logMsg(ctx) {
+    var from = userString(ctx);
+    console.log('->', ctx.message.text, from)
+}
+
+function logOutMsg(ctx, text) {
+    console.log('<-', {
+        id: ctx.chat.id
+    }, text);
+}
+
+//middlewares
+bot.use(async (ctx, next) => {
+    const start = new Date()
+    logMsg(ctx)
+    await next()
+    const ms = new Date() - start
+    console.log('%sms', ms)
+})
+
+//bot replies
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+
+bot.command('/check', async (ctx) => {
+    ctx.reply("checking for updates... ")
     friends = await friendsFetcher.fetchData()
     followers = await followersFetcher.fetchData()
-    msg.reply.text(friends + '\n' + followers)
-});
+    ctx.reply(friends + '\n' + followers)
+    logOutMsg(ctx, friends + '\n' + followers)
+})
 
-bot.start();
+bot.launch()
